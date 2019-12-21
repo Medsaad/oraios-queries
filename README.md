2cabf2483897bbe21516a311550b4b056244549e
ahmed <ahmedthegicoder@gmail.com>
2019-12-21 20:12:05 +0200

    finalizing query execution without specific listing

---
+++ README.md
@@ -0,0 +1,7 @@
+pg-models
+==========
+
+Models & Query Builder for Postgresql
+
+Copyright (c) 2019-2020 Ahmed Saad Zaghloul (ahmedthegicoder@gmail.com)
+MIT License
--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const pgConn = require("./lib/connection");
+const pgConnection = require("./lib/connection");
 const Model = require("./lib/model");
 
+/**
+ * Copyright (c) 2019-2020 Ahmed Saad Zaghloul (ahmedthegicoder@gmail.com)
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * README.md file in the root directory of this source tree.
+ */
 
-module.exports = { pgConn, Model };
+module.exports = { pgConnection, Model };
